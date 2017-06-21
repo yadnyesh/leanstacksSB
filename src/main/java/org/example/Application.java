526a2b403ec7e3000d0c0a25603c942b0dfa2f06
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-06-21 19:19:06 +0530

    Adding Gitignore

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -11,6 +12,6 @@ public class Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(Application.class, args);
     }
 }
