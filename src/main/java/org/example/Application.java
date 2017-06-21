a59226e945a561e3121de1fcbc8b824891bd4b63
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-06-21 19:16:38 +0530

    First Commit

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Main Application
+ *
+ */
+@SpringBootApplication
+public class Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
